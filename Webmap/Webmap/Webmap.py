3772f435907f0a521ec08f594ce68458cc154ab7
Mark <markjcarroll@hotmail.co.uk>
2018-08-29 20:00:47 +0100

    Adding dynamic colours to icons

--- Webmap/Webmap/Webmap.py
+++ Webmap/Webmap/Webmap.py
@@ -8,11 +8,20 @@ lon = list(data["LON"])
 nme = list(data["NAME"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el)+ " m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
