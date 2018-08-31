75c7efa08ef44f4b5d2fe49d839c3541d68f9f44
Mark <markjcarroll@hotmail.co.uk>
2018-08-31 21:56:53 +0100

    added colour grading by population of country

--- Webmap/Webmap/Webmap.py
+++ Webmap/Webmap/Webmap.py
@@ -23,7 +23,9 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln], popup=str(el)+ " m", radius=12, fill_color=color_producer(el), fill=True, fill_opacity=0.7, color='grey'))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+                            style_function=lambda x:{'fillColor':'green' if x ['properties']['POP2005']<10000000 
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 200000000 else 'red'}))
 map.add_child(fg)
 
 map.save("Map1.html")
